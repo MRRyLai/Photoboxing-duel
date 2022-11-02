8ea7053f61095f4abfb0e4f4c3e2045de62dcac4
MRRyLai <102565867+MRRyLai@users.noreply.github.com>
2022-11-02 13:22:36 +0800

    MySQL data
    upload MySQL account,gameplay,rank dataset.
    upload related .php fromat data

---
+++ getuserinfo.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['email']) && isset($_POST['password'])){
+    // Include the necessary files
+    require_once "conn.php";
+    require_once "validate.php";
+    // Call validate, pass form data as parameter and store the returned value
+    $email = validate($_POST['email']);
+    $password = validate($_POST['password']);
+    // Create the SQL query string
+    $sql = "select * from users where email='$email' and password='" . md5($password) . "'";
+    // Execute the query
+    $result = $conn->query($sql);
+    // If number of rows returned is greater than 0 (that is, if the record is found), we'll print "success", otherwise "failure"
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " Name: " . $row["name"]. "\n";
+        }
+    } else{
+        echo "0 results";
+    }
+}
+?>
+
---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])){
+    // Include the necessary files
+    require_once "conn.php";
+    require_once "validate.php";
+    // Call validate, pass form data as parameter and store the returned value
+    $name = validate($_POST['name']);
+    $email = validate($_POST['email']);
+    $password = validate($_POST['password']);
+    // Create the SQL query string. We'll use md5() function for data security. It calculates and returns the MD5 hash of a string
+    $sql = "insert into users values('','$name','$email', '" . md5($password) . "')";
+    // Execute the query. Print "success" on a successful execution, otherwise "failure".
+    if(!$conn->query($sql)){
+        echo "failure";
+    }else{
+        echo "success";   
+    }
+}
+?>
